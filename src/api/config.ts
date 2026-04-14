b063f962fe9ddf05891a2d9042c55bce39aaa733
박태규 Park Tae Kyu <ptq124@naver.com>
2026-04-14 22:32:19 +0900

    chore: url설정

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,4 @@
 export const API_MODE: 'server' | 'local' = 'server';
 
-export const BASE_URL = 'http://3.36.132.253:8000/api/v1';
+// export const BASE_URL = 'http://3.36.132.253:8000/api/v1';
+export const BASE_URL = '/api/v1';
