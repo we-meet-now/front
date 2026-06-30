d7657773c6fa28a7185210cdefbd6897d7dc1bbb
박태규 Park Tae Kyu <ptq124@naver.com>
2026-06-30 20:48:20 +0900

    chore: 채팅 API 도메인 수정

--- src/api/config.ts
+++ src/api/config.ts
@@ -7,8 +7,8 @@ export const BASE_URL = 'https://13.124.208.2.sslip.io/api/v1';
 export const AUTH_URL = 'https://13.124.208.2.sslip.io/api/auth/v1';
 
 // 웹소켓 통신
-export const WS_URL = 'https://13.124.208.2.sslip.io/api/chat/v1/message/ws-chat';
+export const WS_URL = 'https://backend-chat-service.onrender.com/api/chat/v1/message/ws-chat';
 // export const WS_URL = 'http://13.124.208.2/api/chat/v1/message/ws-chat';
 
 // chat API
-export const CHAT_URL = 'https://13.124.208.2.sslip.io/api/chat/v1';
+export const CHAT_URL = 'https://backend-chat-service.onrender.com/api/chat/v1';
